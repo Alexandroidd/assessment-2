9006e217a421e78e34b88c7e0a83ac0f21d58241
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-26 15:28:30 -0600

    adds win feature for both player One and Player two using finish line on right side of screen

--- main.js
+++ main.js
@@ -4,47 +4,75 @@
 ///Player One Ship Data///
 var shipOne = document.getElementById('shipOne');
 var moveOne = 100;
-var shipOneRight = window.getComputedStyle(shipOne, null).getPropertyValue('right');
+// var shipOneRight = window.getComputedStyle(shipOne, null).getPropertyValue('margin-right');
+// var shipOnePosition = parseInt(shipOneRight,10);
+// console.log(shipOnePosition);
 //------------//
 
 
 //Player Two Ship Data///
 var ShipTwo = document.getElementById('shipTwo');
 var moveTwo = 100;
+// var shipTwoRight = window.getComputedStyle(shipTwo,null).getPropertyValue('right');
+// var shipTwoPosition = parseInt(shipTwoRight,10);
+// console.log(shipTwoPosition);
+
+//-------------//
+//Finish Line Data//
 var finishline = document.getElementById('finishLine');
-var finishLineLeft = window.getComputedStyle(finishLine, null).getPropertyValue('left');
-var finishLinePosition = parseInt(finishLineLeft,10);
+var finishLineRight = window.getComputedStyle(finishLine, null).getPropertyValue('left');
+var finishLinePosition = parseInt(finishLineRight,10);
 console.log(finishLinePosition);
-
-// var finishLinePosition = .getAttribute('top');
-
-// var finishLinePosition = parseInt(finishLine, 10);
-var shipOnePosition = shipOne.style.right;
-var shipTwoPosition = shipTwo.style.right;
+//--------------//
 
 
 
+//Player One Ship Controls //
 document.addEventListener('keydown', function(event) {
 	if(event.code === "KeyL") {
 		console.log("I'm Moving right!");
 		shipOne.style.left = moveOne + "px" ;
+		//----
+		var shipOneRight = window.getComputedStyle(shipOne, null).getPropertyValue('left');
+		var shipOnePosition = parseInt(shipOneRight,10);
+		console.log(shipOnePosition);
+		console.log(finishLinePosition);
+		//---//
+		winCheckOne = function() {
+			if (shipOnePosition >= finishLinePosition) {
+			alert("PLAYER One WINS!");
+			}
+		};
+		winCheckOne();
+		//----
 		moveOne += 100;
 	}
 });
+//---------------//
 
-winCheck = function() {
-	if (shipOnePosition === finishLine) {
-		alert("PLAYER WINS!");
-	}
-};
-winCheck();
 
+
+// Player Two Ship Controls ///
 document.addEventListener('keydown', function(event) {
 	if(event.code === "KeyA") {
 		console.log("I'm Moving right Too!");
 		shipTwo.style.left = moveTwo + "px";
+		//---//
+		var shipTwoRight = window.getComputedStyle(shipTwo,null).getPropertyValue('left');
+		var shipTwoPosition = parseInt(shipTwoRight,10);
+		console.log(shipTwoPosition);
+		//--//
+		winCheckTwo = function() {
+			if (shipTwoPosition >= finishLinePosition) {
+			alert("PLAYER Two WINS!");
+			shipTwoPosition = 0;
+			shipOnePosition = 0;
+			}
+		};
+		winCheckTwo();
+		//--//
 		moveTwo += 100;
 	}
-
-
 });
+//----------------//
+
