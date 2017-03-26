3adfaacae638be0b28806d259270882d48fad80a
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-26 14:33:17 -0600

    adds finish line and ability to locate finish line's left edge. Now adding detection for each ship's right edge.

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+
+// Ship Movement Data/////
+
+///Player One Ship Data///
 var shipOne = document.getElementById('shipOne');
 var moveOne = 100;
+var shipOneRight = window.getComputedStyle(shipOne, null).getPropertyValue('right');
+//------------//
+
+
+//Player Two Ship Data///
 var ShipTwo = document.getElementById('shipTwo');
 var moveTwo = 100;
+var finishline = document.getElementById('finishLine');
+var finishLineLeft = window.getComputedStyle(finishLine, null).getPropertyValue('left');
+var finishLinePosition = parseInt(finishLineLeft,10);
+console.log(finishLinePosition);
+
+// var finishLinePosition = .getAttribute('top');
+
+// var finishLinePosition = parseInt(finishLine, 10);
+var shipOnePosition = shipOne.style.right;
+var shipTwoPosition = shipTwo.style.right;
+
+
 
 document.addEventListener('keydown', function(event) {
 	if(event.code === "KeyL") {
@@ -10,6 +31,13 @@ document.addEventListener('keydown', function(event) {
 		moveOne += 100;
 	}
 });
+
+winCheck = function() {
+	if (shipOnePosition === finishLine) {
+		alert("PLAYER WINS!");
+	}
+};
+winCheck();
 
 document.addEventListener('keydown', function(event) {
 	if(event.code === "KeyA") {
